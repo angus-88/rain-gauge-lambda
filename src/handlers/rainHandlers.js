4a80d516abf8382e6aab9f46cc334d073b445c70
Angus <angus.h88@outlook.com>
2020-10-25 16:13:03 +0000

    :fix remove ts from rain handler

--- src/handlers/rainHandlers.js
+++ src/handlers/rainHandlers.js
@@ -1,16 +1,15 @@
-import Alexa, { getRequest } from 'ask-sdk';
-import { IntentRequest } from 'ask-sdk-model';
+const Alexa = require('ask-sdk');
 
-import { addRain, getTotalForCurrentDay, getTotalForCurrentMonth } from '../dynamoHelper';
+const { addRain, getTotalForCurrentDay, getTotalForCurrentMonth } = require('../dynamoHelper');
 
 const AddRainHandler = {
-  canHandle(handlerInput: Alexa.HandlerInput) {
+  canHandle(handlerInput) {
     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
       && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AddRain';
   },
-  async handle(handlerInput: Alexa.HandlerInput) {
+  async handle(handlerInput) {
     console.log(JSON.stringify(handlerInput, null, 2));
-    const request = getRequest<IntentRequest>(handlerInput.requestEnvelope);
+    const request = Alexa.getRequest(handlerInput.requestEnvelope);
     const amount = request.intent.slots?.amount.value || '0';
     let response = '';
     try {
@@ -29,11 +28,11 @@ const AddRainHandler = {
 };
 
 const GetRainForMonth = {
-  canHandle(handlerInput: Alexa.HandlerInput) {
+  canHandle(handlerInput) {
     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
       && Alexa.getIntentName(handlerInput.requestEnvelope) === 'RainForMonth';
   },
-  async handle(handlerInput: Alexa.HandlerInput) {
+  async handle(handlerInput) {
     const total = await getTotalForCurrentMonth();
 
     return handlerInput.responseBuilder
@@ -43,20 +42,21 @@ const GetRainForMonth = {
 };
 
 const GetRainForToday = {
-  canHandle(handlerInput: Alexa.HandlerInput) {
+  canHandle(handlerInput) {
     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
       && Alexa.getIntentName(handlerInput.requestEnvelope) === 'RainForToday';
   },
-  async handle(handlerInput: Alexa.HandlerInput) {
+  async handle(handlerInput) {
     const total = await getTotalForCurrentDay();
 
     return handlerInput.responseBuilder
       .speak(`Today it has rained ${total} millimetres`)
       .getResponse();
   }
-}
+};
 
 module.exports = {
   AddRainHandler,
   GetRainForMonth,
-}
+  GetRainForToday
+};
