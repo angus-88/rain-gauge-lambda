d50fcd9d71b2d5ba1a71036ba6ee8c7ddc0f536d
Angus <angus.h88@outlook.com>
2021-01-10 12:37:56 +0000

    generic rain for timespan parser

--- src/handlers/rainHandlers.ts
+++ src/handlers/rainHandlers.ts
@@ -4,8 +4,8 @@ import moment from 'moment';
 
 import * as Alexa from 'ask-sdk';
 
-import { addRain, getAllRainRecords, getTotalForTimeFrame, getWettestTimeSpan } from '../dynamoHelper';
-import { groupRecordsByTimeSpan } from '../utilities';
+import { addRain, getTotalForTimeFrame, getWettestTimeSpan } from '../dynamoHelper';
+import { getTotalFromItems } from '../utilities';
 
 const AddRainHandler = {
   canHandle(handlerInput: HandlerInput) {
@@ -78,6 +78,84 @@ const GetRainForPreviousMonth = {
   }
 }
 
+const GetRainForTimespan = {
+  /*
+    2020
+    last week
+    2020-W55
+    2020-10
+    2020-10-25
+  */
+  canHandle(handlerInput: HandlerInput) {
+    return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+      && Alexa.getIntentName(handlerInput.requestEnvelope) === 'RainForPreviousTimeSpan';
+  },
+  async handle(handlerInput: HandlerInput) {
+    const request = Alexa.getRequest<IntentRequest>(handlerInput.requestEnvelope);
+    const requestDate = request.intent.slots?.date.value;
+    console.log('requestDate: ', requestDate);
+
+    const dateMoment = moment();
+    let timeSpan: moment.unitOfTime.StartOf = 'year';
+
+    const dateParts = requestDate?.split('-');
+    if (dateParts?.length === 0) {
+      const year = Number.parseInt(dateParts[0]);
+      /// Year or un recognised
+      if (!Number.isNaN(year)) {
+        // Year
+        dateMoment.year(year);
+        console.log(`get total for ${year}`);
+        timeSpan = 'year';
+      } else {
+         // NaN
+        if (dateParts[0] === 'last week') {
+          dateMoment.subtract(1, 'week');
+          timeSpan = 'week';
+          console.log(`get total for last week`);
+      }
+       }
+    } else if (dateParts?.length === 2) {
+      const year = Number.parseInt(dateParts[0]);
+      dateMoment.year(year);        
+
+      // Week or Month
+      if (dateParts[1].startsWith('W')) {
+        // Week
+        const weekNumber = Number.parseInt(dateParts[1].slice(1));
+        console.log(`week number ${weekNumber}`);
+
+        dateMoment.weekYear(weekNumber);
+        timeSpan = 'week';
+      } else {
+        // Month
+        const month = Number.parseInt(dateParts[1]);
+        dateMoment.month(month - 1); // Moment months are indexed from 0, so april = 3
+        timeSpan = 'month';
+        console.log(`get total for ${dateMoment.format('MMMM YYYY')}`);
+      }
+    } else if (dateParts?.length === 3) {
+      // Exact date
+      dateMoment.set({ 
+        year: Number.parseInt(dateParts[0]),
+        month: Number.parseInt(dateParts[1]) -1,
+        date: Number.parseInt(dateParts[2]),
+      })
+
+      console.log('Parsed Moment: ', dateMoment.toISOString());
+
+      timeSpan = 'day';
+    }
+
+    const total = await getTotalForTimeFrame(dateMoment, timeSpan);
+
+    return handlerInput.responseBuilder
+      .speak(`The total is ${total}`)
+      .reprompt('any other rain questions')
+      .getResponse();
+  }
+}
+
 const GetWettestTimeSpan = {
   canHandle(handlerInput: HandlerInput) {
     return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
@@ -102,5 +180,6 @@ module.exports = {
   GetRainForCurrentTimeSpan,
   GetRainForPreviousMonth,
   GetWettestTimeSpan,
+  GetRainForTimespan,
 };
 
