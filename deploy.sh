52b8993472f3c83bd27bdf7d3b5f5a73dae11f8a
Angus <angus.h88@outlook.com>
2021-02-07 11:43:12 +0000

    :remove dev deps when deploying

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 npm run build
+npm prune --production
 
 zip -r function.zip node_modules/ build/
 
 aws lambda update-function-code --function-name alexa-rain-gauge --zip-file fileb://function.zip
+
+npm install
